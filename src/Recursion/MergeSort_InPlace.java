b2be23b25d43e90933e529b27ff812245a6a7a11
riteshbhardwaj-0001 <riteshbhardwaj00001@gmail.com>
2023-01-04 16:23:32 +0530

    Merge Sort Using Two Different Types

---
+++ src/Recursion/MergeSort_InPlace.java
@@ -0,0 +1,51 @@
+package Recursion;
+
+import java.util.Arrays;
+
+public class MergeSort_InPlace {
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,1};
+        mergeSort(arr,0,arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+    private static void mergeSort(int[] arr, int s, int e) {
+        if(e-s==1){
+            return;
+        }
+        int mid=(s+e)/2;
+
+        mergeSort(arr,s,mid);
+        mergeSort(arr,mid,e);
+
+        merge(arr,s,mid,e);
+    }
+    private static void merge(int[] arr, int s, int m, int e) {
+        int[] mix=new int[e-s];
+        int i=s;
+        int j=m;
+        int k=0;
+        while(i<m && j<e){
+            if(arr[i]<arr[j]){
+                mix[k]=arr[i];
+                i++;
+            }else{
+                mix[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<m){
+            mix[k]=arr[i];
+            i++;
+            k++;
+        }
+        while(j<e){
+            mix[k]=arr[j];
+            j++;
+            k++;
+        }
+        for (int l=0;l<mix.length;l++){
+            arr[s+l]=mix[l];
+        }
+    }
+}
