69d7c9d476afe3551d9a7fa4e4a978f2adef1d4f
riteshbhardwaj-0001 <riteshbhardwaj00001@gmail.com>
2023-02-18 13:47:39 +0530

    Searching In 2d array

--- src/LinearSearch/SearchIn2DArray.java
+++ src/LinearSearch/SearchIn2DArray.java
@@ -13,6 +13,25 @@ public class SearchIn2DArray {
             }
         }
         System.out.println("Enter the value you want to search:");
-        
+        int value=sc.nextInt();
+
+        if(search(arr,value)){
+            System.out.println("Value Found");
+        }else{
+            System.out.println("Not Found");
+        }
+    }
+    private static boolean search(int[][] arr, int value) {
+        if(arr.length==0){
+            return false;
+        }
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]==value){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
